1950b8270998bea21bd98d5cabab7f98be0caa21
Chrystian Lozano <chrystianlozano@icloud.com>
2019-02-28 07:35:52 -0600

    Se agrego el resto de las unidades academicas

--- jsResultados/unit.js
+++ jsResultados/unit.js
@@ -2,8 +2,7 @@ const output = document.querySelector('#listResult');
 const dateTest = document.querySelector('#dateTest');
 const alertMess = document.querySelector('#alertMess');
 const titleUnit = document.querySelector('#titleUnit');
-
-      var unidad = document.getElementById('unidad');
+var unidad = document.getElementById('unidad');
       unidad.onchange=function(){
         if (unidad.value == 4) {
           const id = '1imqj0vgkt4YuYpZpa58SpCM0AipphSJTeENljz3CPbU'
@@ -13,8 +12,15 @@ const titleUnit = document.querySelector('#titleUnit');
           const id = '1JcgcsYXEDuaRSzEVquN3EPibYc7bo0n5Mg9I5sBDy8I'
           mixtaLista(id);
         }
+        if (unidad.value == 2) {
+          const id = '1ozESOG_vqhE3LuXTMDKvtPiHB0oXqa4Ey6nNK2_9LZ0'
+          mixtaLista(id);
+        }
+        if (unidad.value == 3) {
+          const id = '110bJ76KL7ufcu8Vor5BCcymWpX85zJQnpz5qSQpUPxA'
+          mixtaLista(id);
+        }
       }
-      document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
 var elems = document.querySelectorAll('select');
-//var instances = M.FormSelect.init(elems, options);
 });
