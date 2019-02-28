757d8d24194a15504ed6339e0a83ced506011ea3
Chrystian Lozano <chrystianlozano@icloud.com>
2019-02-28 08:42:40 -0600

    algunos ajustes

--- jsResultados/resultMixta.js
+++ jsResultados/resultMixta.js
@@ -11,7 +11,6 @@ function mixtaLista(id) {
                     dateTest.innerHTML = entry[0].gsx$matricula.$t;
                     alertMess.innerHTML = '<h6 class="card-panel light-blue darken-4 z-depth-1 white-text" id="alertMess">' + entry[0].gsx$mensaje.$t + '</h6>';
                     titleUnit.innerHTML = entry[0].gsx$titulo.$t;
-
                     for(let x=1;x<entry.length;x++){
                         let matricula = entry[x].gsx$matricula.$t;
                         let mensaje = entry[x].gsx$mensaje.$t;
