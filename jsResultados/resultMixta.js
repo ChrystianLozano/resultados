40ccca2716aaef1a4244d3dcaf95be9471b9accf
Chrystian Lozano <chrystianlozano@icloud.com>
2019-03-07 10:02:17 -0600

    Se creo una funcion asincrona

--- jsResultados/resultMixta.js
+++ jsResultados/resultMixta.js
@@ -4,20 +4,17 @@ function mixtaLista(id) {
     dateTest.innerHTML = '';
     alertMess.innerHTML = '';
     titleUnit.innerHTML = '';
-    fetch(url).then(function (response) {
-                    return response.json()
-                }).then(function (data) {
-                    let entry = data.feed.entry;
-                    dateTest.innerHTML = entry[0].gsx$matricula.$t;
-                    alertMess.innerHTML = '<h6 class="card-panel light-blue darken-4 z-depth-1 white-text" id="alertMess">' + entry[0].gsx$mensaje.$t + '</h6>';
-                    titleUnit.innerHTML = entry[0].gsx$titulo.$t;
-                    for(let x=1;x<entry.length;x++){
-                        let matricula = entry[x].gsx$matricula.$t;
-                        let mensaje = entry[x].gsx$mensaje.$t;
-                        listResult.innerHTML += '<tr><td class="center-align">'+matricula+'</td><td>' +mensaje+'</td></tr>';
-                    }
-                })
-    .catch(function(error){
-        console.log(error);
-    })
+    (async function load(){
+      const response = await fetch(url);
+      const data = await response.json()
+      let entry = data.feed.entry;
+      dateTest.innerHTML = entry[0].gsx$matricula.$t;
+      alertMess.innerHTML = '<h6 class="card-panel light-blue darken-4 z-depth-1 white-text" id="alertMess">' + entry[0].gsx$mensaje.$t + '</h6>';
+      titleUnit.innerHTML = entry[0].gsx$titulo.$t;
+      for(let x=1;x<entry.length;x++){
+          let matricula = entry[x].gsx$matricula.$t;
+          let mensaje = entry[x].gsx$mensaje.$t;
+          listResult.innerHTML += '<tr><td class="center-align">'+matricula+'</td><td>' +mensaje+'</td></tr>';
+      }
+    })();
 }
