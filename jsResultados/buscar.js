8eb398ad03267c980f2f7dc0cd287a20252aa678
Chrystian Lozano <chrystianlozano@icloud.com>
2019-03-20 11:58:01 -0600

    Iniciando el backend del buscador

---
+++ jsResultados/buscar.js
@@ -0,0 +1,16 @@
+function enlista() {
+  console.log('Clicking');
+  let matriculaID = document.getElementById('icon_prefix').value;
+  console.log(matriculaID);
+  let lobuscado = document.querySelector('#lobuscado');
+  let elresultado = document.querySelector('#elresultado')
+  lobuscado.innerHTML = matriculaID;
+
+  if (matriculaID.length > 1 && matriculaID.length < 8 ) {
+    elresultado.innerHTML = 'La matricula debe de tener 8 digitos';
+  }
+  if (matriculaID.length === 0){
+    elresultado.innerHTML = 'Es necesario que Introdusca una matricula';
+  }
+  
+}
--- jsResultados/resultMixta.js
+++ jsResultados/resultMixta.js
@@ -8,16 +8,17 @@ function mixtaLista(id) {
     (async function load(){
       output.innerHTML = '<div class="progress"><div class="indeterminate"></div></div>';
       const response = await fetch(url);
-      const data = await response.json()
+      const data = await response.json();
       let entry = data.feed.entry;
       buscar.innerHTML = `<div class="input-field col s6">
           <i class="material-icons prefix">search_circle</i>
-          <input id="icon_prefix" type="text" class="validate">
+          <input id="icon_prefix" type="number" class="validate" required>
           <label for="icon_prefix">Matricula</label>
         </div>
         <div class="input-field col s6">
-          <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Buscar</a>
+          <a class="waves-effect waves-light btn modal-trigger" href="#modal1" id="botonBuscar">Buscar</a>
           </div>`;
+          document.getElementById('botonBuscar').addEventListener('click', enlista);
       dateTest.innerHTML = 'Examen aplicado el ' + entry[0].gsx$matricula.$t;
       alertMess.innerHTML = '<h6 class="card-panel light-blue darken-4 z-depth-1 white-text" id="alertMess">' + entry[0].gsx$mensaje.$t + '</h6>';
       titleUnit.innerHTML = entry[0].gsx$titulo.$t;
